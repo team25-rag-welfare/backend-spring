2b6ced211a8936328a2a09391c0d47dbd4ba375d
gganb <gksql0801@naver.com>
2026-05-03 05:06:12 +0900

    chore : 프로젝트 초기 세팅

---
+++ src/main/java/com/github/ggab/backendspring/BackendSpringApplication.java
@@ -0,0 +1,15 @@
+package com.github.ggab.backendspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+@SpringBootApplication
+public class BackendSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/ggab/backendspring/BackendSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.ggab.backendspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
