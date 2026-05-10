1554df8f6ab2d3b676920cf347e125604257a78b
BaekSR6 <beaksonglul@gmail.com>
2026-05-11 02:58:17 +0900

    일단 한번 체크포인트 남기는 거라 신경 안 쓰셔도 됩니다3

--- src/main/java/com/sancheck/backend/BackendSpringApplication.java
+++ src/main/java/com/sancheck/backend/BackendSpringApplication.java
@@ -2,12 +2,15 @@ package com.sancheck.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.mongodb.autoconfigure.MongoAutoConfiguration;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
 @EnableMongoAuditing
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+    MongoAutoConfiguration.class
+})
 public class BackendSpringApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/sancheck/backend/domain/config/MongoConfig.java
+++ src/main/java/com/sancheck/backend/domain/config/MongoConfig.java
@@ -1,5 +1,7 @@
 package com.sancheck.backend.domain.config;
 
+import com.mongodb.client.MongoClient;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.core.MongoTemplate;
@@ -9,11 +11,24 @@ import com.mongodb.client.MongoClients;
 @Configuration // "스프링아, 딴 거 다 무시하고 이 설정부터 무조건 읽어라!"
 public class MongoConfig {
 
-    @Bean
-    public MongoTemplate mongoTemplate() {
-        // 🚨 여기에 진짜 아틀라스 주소를 싹 붙여넣으십쇼!
-        String myAtlasUri = "mongodb+srv://jefndb:1111@cluster0.s7uiual.mongodb.net/chat_messages?retryWrites=true&w=majority";
+    @Value("${spring.data.mongodb.uri}")
+    private String mongoUri;
 
-        return new MongoTemplate(new SimpleMongoClientDatabaseFactory(MongoClients.create(myAtlasUri), "chat_messages"));
+    @Bean
+    public MongoClient mongoClient() {
+
+        return MongoClients.create(mongoUri);
     }
+
+    @Bean
+    public MongoTemplate mongoTemplate(MongoClient mongoClient){
+        return new MongoTemplate(mongoClient, "chat_messages");
+    }
+
+
+
+
 }
+
+
+
